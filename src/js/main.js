17c215a48c596590ae69262763f0402c272fba7c
KanasBernardo <98572675+KanasBernardo@users.noreply.github.com>
2022-07-07 01:03:10 -0600

    Gran Actualizacion

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,13 @@
 import "../css/main.css";
 
+//para el menu hamburgueza
+const bMore = document.querySelector("#bMore");
+const links = document.querySelector("#links");
+bMore.addEventListener("click", (e) =>{
+    links.classList.toggle("collapsed");
+});
+//Inicio del codigo para la agenda
+
 let events = [];
 let arr = []; //para cargar informacion
 
@@ -7,6 +15,63 @@ const eventName = document.querySelector("#eventName");
 const eventDate = document.querySelector("#eventDate");
 const buttonAdd = document.querySelector("#bAdd");
 const eventsContainer = document.querySelector("#eventsContainer");
+const Cusar = document.querySelector("#usar");
+const contactame = document.querySelector("#contact");
+
+Cusar.addEventListener("click", (e) =>{
+    return Swal.fire({
+        title: '<h5 class="tale">¿Como Funciona se usa la agenda?</5>',
+        icon: 'info',
+        html:
+        '<p class="contenidoAlert">La agenda funciona como un contador de días, almacenando los datos en memeria local, es decir que se eliminaran los datos hasta que borres la cache de tu navegador o elimando lo agendado con el boton de eliminar</p><br><br>' +
+        '<p class="contenidoAlert">Si descargas la aplicación para dispositivos mobiles, de la misma manera la aplicación utilizara memoria de tu celular y asi mismo se borrara lo agendado con el boton de eliminar o ya sea borrando los daros de la aplicación</p>',
+        //showCloseButton: true,
+        focusConfirm: false,
+        footer: '<span class="spAlert">¡Información Importante!',
+        //background: '#7760FF',
+        //width: '90%',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        customClass: {
+            popup: 'tamAlerta'
+        }
+      })
+});
+
+contactame.addEventListener("click", (e) =>{
+    return Swal.fire({
+        title: '<h6 class="tale">¡Hola!, ¿Crees que puedo mejorar algo?</h6 >',
+        html:
+            '<p class="contenidoAlert">Tu comentario me seria de mucha ayuda para mejorar esta agenda</p><br><br>'+
+            '↓Contactame↓<br>',
+        showCancelButton: true,
+        showDenyButton: true,
+        confirmButtonColor: '#5B94D5',
+        cancelButtonColor: '#A0A0A0',
+        confirmButtonText: 'Facebook',
+        denyButtonText: `Instagram`,
+        padding: '1em',
+        color: '#816add',
+        background: '#fff url(/images/trees.png)',
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("/images/nyan-cat.gif")
+          left top
+          no-repeat
+        `,
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        customClass: {
+            popup: 'tamAlerta'
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+          location.href = "https://www.facebook.com/xdjBrKasPT";
+        } else if (result.isDenied) {
+          location.href = "https://www.instagram.com/bernard_k89/";
+        }
+      })
+});
 
 const json = load();
 
@@ -35,7 +100,17 @@ function addEvent(){
     }
 
     if(dateDiff(eventDate.value) < 0){
-        return;
+        return Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'No puedes Agendar actividades en fechas que ya pasaron!',
+            footer: '<p>¡Intenta con una nueva Fecha!</p>',
+            background: '#E5E7E9',
+            
+            customClass: {
+                popup: 'tam'
+            }
+          });
     }
 
     const newEvent = {
